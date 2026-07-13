f7b507259aa781c7b3c5eb9979f4e0752bdd9aac
João Pedro Jensen Farias <jp.123pedrofarias@gmail.com>
2026-07-13 16:48:25 -0300

    Aula 13.07.2026

---
+++ Aula 13.07.2026/cabecalho.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>&diams; Sistema Petnac &diams;</title>
+	<link rel="icon" href="img/icon.png">
+	<meta name="author" content="FATALIIIS">
+	<meta name="keywords" content="petshop,serviços,agendamento,">
+</head>
+<body>
