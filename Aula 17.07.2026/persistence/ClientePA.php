d8bb1889f9494df36ff4109f7a86b26eb46af6b6
João Pedro Jensen Farias <jp.123pedrofarias@gmail.com>
2026-07-17 17:24:41 -0300

    Aula 17.07.2026

---
+++ Aula 17.07.2026/persistence/ClientePA.php
@@ -0,0 +1,52 @@
+<?php require_once 'Banco.php';
+
+class ClientePA{
+	private $con;
+
+	public function __construct()
+	{
+		$this->con=new Banco();
+	}
+	public function cadastrar($cliente)
+	{
+		$sql="INSERT INTO cliente(login,cpf,data_nasci,senha,nome,endereco,telefone) VALUES(?,?,?,?,?,?,?)";
+		$campos=[
+			$cliente->getLogin(),
+			$cliente->getCpf(),
+			$cliente->getDataNasci(),
+			$cliente->getSenha(),
+			$cliente->getNome(),
+			$cliente->getEndereco(),
+			$cliente->getTelefone(),
+		];
+		$resposta=$this->con->executar($sql,$campos,"sisssss");
+		$this->con->desconectar();
+		return $resposta;
+	}
+	public function verificar($campo,$valor)
+	{
+		$sql="SELECT $campo FROM cliente WHERE $campo=?";
+		$consulta=$this->con->consultar($sql,[$valor],"s");
+		if (!$consulta) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public function logar($usuario,$senha)
+	{
+		$sql="SELECT cod_cli,login,senha FROM cliente WHERE login=?";
+		$consulta=$this->con->consultar($sql,[$login],'s');
+		if(!$consulta){
+			return false;
+		}else{
+			$linha=$consulta->fetch_assoc();
+			if (password_verify($senha, $linha['senha'])) {
+				return $linha['cod_cli'];
+			}else{
+				return false;
+			}
+		}
+	}
+}	
+?>
